0cb361efd73a77419061523d8f7bd5ee772b326b
Vyacheslav Napadovsky <napadovskiy@gmail.com>
2024-08-31 17:55:45 +0900

    Simplify

--- dllthread-win32.cpp
+++ dllthread-win32.cpp
@@ -3,27 +3,17 @@
 
 struct dllthread::InitStruct {
 	std::function<void()> m_fn;
-	HANDLE m_threadStarted, m_threadEnded;
-	volatile bool m_threadCancelled;
+	HANDLE m_threadStarted;
 	InitStruct(std::function<void()>&& fn) :
 		m_fn(std::move(fn)),
-		m_threadStarted(INVALID_HANDLE_VALUE),
-		m_threadEnded(INVALID_HANDLE_VALUE),
-		m_threadCancelled(false)
+		m_threadStarted(INVALID_HANDLE_VALUE)
 	{
 	}
 	~InitStruct() {
 		CloseHandle(m_threadStarted);
 		m_threadStarted = INVALID_HANDLE_VALUE;
-		CloseHandle(m_threadEnded);
-		m_threadEnded = INVALID_HANDLE_VALUE;
 	}
 };
-
-dllthread::~dllthread() {
-	if (joinable())
-		join();
-}
 
 void dllthread::join() {
 	if (!joinable())
@@ -36,32 +26,25 @@ void dllthread::join() {
 	// I give thread up to 3 seconds to start, if thread is not started in this time,
 	// then most probably we are in DllMain function, thus, we are in deadlock situation, so
 	// we need to emulate successfull thread join via thread termination.
-	HANDLE handles[2] = { m_threadStarted, m_threadEnded };
+	HANDLE handles[2] = { m_threadStarted, m_thread };
 	auto ret = WaitForMultipleObjects(sizeof(handles) / sizeof(*handles), handles, FALSE, 3000);
 	switch (ret) {
 		// m_threadStarted
 		case WAIT_OBJECT_0:
 			// thread started thus, going to default routine
-			// check m_thread handle first (thread might be interrupted by system before)
-			// and if thread is running then wait for the m_handle
-			if (WaitForSingleObject(m_thread, 0) == WAIT_TIMEOUT)
-				WaitForSingleObject(m_threadEnded, INFINITE);
+			WaitForSingleObject(m_thread, INFINITE);
 			break;
 
-		// m_threadEnded
+		// m_thread
 		case (WAIT_OBJECT_0 + 1) :
-			// success, thread runned & finished
+			// success, thread run & finished
 			break;
 
-		// in case of timeout or error, just detach
 		case WAIT_TIMEOUT:
-		default:
 			// error, but we pretend that it is success, because thread wasn't even started yet.
 			// force thread termination and pretend we are ok.
-			m_initstruct->m_threadCancelled = true;
-			MemoryBarrier();	// let all CPU receive this value
-			SuspendThread(m_thread);
 			TerminateThread(m_thread, 0);
+			delete m_initstruct;
 			break;
 	}
 	detach();
@@ -72,41 +55,32 @@ void dllthread::detach() {
 		throw std::runtime_error("This thread is null or has been detached already");
 	CloseHandle(m_thread);
 	CloseHandle(m_threadStarted);
-	CloseHandle(m_threadEnded);
 	reset(*this);
 }
 
 DWORD WINAPI dllthread::threadstart(LPVOID param) {
-	auto init = reinterpret_cast<InitStruct*>(param);
-	// here's a check for deadlock. See SetEvent(m_threadEnded) in dllthread::join()
-	SetEvent(init->m_threadStarted);
-	if (!init->m_threadCancelled)
-		init->m_fn();
-	// TODO: here's the bottleneck. If you're joining thread within DLL_PROCESS_DETACH, this code below may not be
-	// fully finish atomically. We need to SetEvent and ExitThread atomically before DLL will be unloaded.
-	// This is a bug. A bad workaround would be to Sleep() for some amount of time, to let the thread to finish
-	// or allow memory leak for init structure with SuspendThread() and TerminateThread() in join implementation.
-	SetEvent(init->m_threadEnded);
-	delete init;
+	std::function<void()> fn;
+	{
+		auto init = reinterpret_cast<InitStruct*>(param);
+		SetEvent(init->m_threadStarted);
+		fn = std::move(init->m_fn);
+		delete init;
+	}
+	fn();
 	return 0;
 }
 
 void dllthread::init(std::function<void()>&& fn) {
-	auto init = new InitStruct(std::forward<std::function<void()>>(fn));
+	auto init = new InitStruct(std::move(fn));
 	m_initstruct = init;
 	try {
-		;
-		if ((m_threadStarted = CreateEvent(nullptr, false, false, nullptr)) == nullptr ||
-			(m_threadEnded = CreateEvent(nullptr, false, false, nullptr)) == nullptr)
+		if ((m_threadStarted = CreateEvent(nullptr, false, false, nullptr)) == nullptr)
 		{
 			m_threadStarted = INVALID_HANDLE_VALUE;
-			m_threadEnded = INVALID_HANDLE_VALUE;
 			throw std::system_error(0, std::system_category(), "Can't create event");
 		}
 		HANDLE hProcess = GetCurrentProcess();
 		if (!DuplicateHandle(hProcess, m_threadStarted, hProcess, &init->m_threadStarted, 0, FALSE, DUPLICATE_SAME_ACCESS))
-			throw std::system_error(0, std::system_category(), "DuplicateHandle failed");
-		if (!DuplicateHandle(hProcess, m_threadEnded, hProcess, &init->m_threadEnded, 0, FALSE, DUPLICATE_SAME_ACCESS))
 			throw std::system_error(0, std::system_category(), "DuplicateHandle failed");
 		m_thread = CreateThread(nullptr, 0, threadstart, init, 0, &m_id);
 		if (m_thread == nullptr) {
@@ -117,15 +91,7 @@ void dllthread::init(std::function<void()>&& fn) {
 	catch (...) {
 		CloseHandle(m_thread);
 		CloseHandle(m_threadStarted);
-		CloseHandle(m_threadEnded);
 		delete init;
 		throw;
 	}
-}
-
-void dllthread::reset(dllthread& obj) {
-	obj.m_threadStarted = INVALID_HANDLE_VALUE;
-	obj.m_threadEnded = INVALID_HANDLE_VALUE;
-	obj.m_thread = INVALID_HANDLE_VALUE;
-	obj.m_id = 0;
 }
--- dllthread.hpp
+++ dllthread.hpp
@@ -37,58 +37,61 @@ public:
 	typedef HANDLE native_handle_type;
 
 	id get_id() const { return m_id; }
-	native_handle_type native_handle() const { return m_threadEnded; }
+	native_handle_type native_handle() const { return m_thread; }
 	static unsigned int hardware_concurrency() { return std::thread::hardware_concurrency(); }
 	
-	dllthread() { reset(*this); }
+	dllthread() { reset(); }
 
 	template<typename Fn, typename ...Args, typename std::enable_if<!std::is_same<typename std::decay<Fn>::type, dllthread>::value, int>::type* = nullptr>
 	explicit dllthread(Fn&& fn, Args&&... args) {
-		reset(*this);
+		reset();
 		init(std::move(std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...)));
 	}
 
-	~dllthread();
+	~dllthread() {
+		if (joinable())
+			join();
+	}
 
 	dllthread(const dllthread&) = delete;
 	dllthread& operator=(const dllthread&) = delete;
 
 	dllthread(dllthread&& other) :
 		m_threadStarted(other.m_threadStarted),
-		m_threadEnded(other.m_threadEnded),
 		m_thread(other.m_thread),
-		m_id(other.m_id)
+		m_id(other.m_id),
+		m_initstruct(other.m_initstruct)
 	{
-		reset(other);
+		other.reset();
 	}
 
 	dllthread& operator=(dllthread&& other) {
 		if (joinable())
 			join();
 		m_threadStarted = other.m_threadStarted;
-		m_threadEnded = other.m_threadEnded;
 		m_id = other.m_id;
 		m_thread = other.m_thread;
-		reset(other);
+		m_initstruct = other.m_initstruct;
+		other.reset();
 		return *this;
 	}
 
 	void swap(dllthread& other) {
 		std::swap(m_threadStarted, other.m_threadStarted);
-		std::swap(m_threadEnded, other.m_threadEnded);
 		std::swap(m_thread, other.m_thread);
 		std::swap(m_id, other.m_id);
+		std::swap(m_initstruct. other.m_initstruct);
 	}
 
 	bool joinable() const {
-		return m_threadEnded != INVALID_HANDLE_VALUE;
+		return m_thread != INVALID_HANDLE_VALUE;
 	}
 
 	void join();
 	void detach();
 
 private:
-	HANDLE m_threadStarted, m_threadEnded, m_thread;
+	HANDLE m_threadStarted, m_thread;
 	DWORD m_id;
 
 	struct InitStruct;
@@ -96,7 +99,12 @@ private:
 	static DWORD WINAPI threadstart(LPVOID param);
 
 	void init(std::function<void()>&& fn);
-	static void reset(dllthread& obj);
+	void reset() {
+		m_threadStarted = INVALID_HANDLE_VALUE;
+		m_thread = INVALID_HANDLE_VALUE;
+		m_id = 0;
+		m_initstruct = nullptr;
+	}
 };
 
 #else // ifdef _WIN32
